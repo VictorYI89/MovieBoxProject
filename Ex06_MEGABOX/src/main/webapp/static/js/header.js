5c8d7dfca6935f37e59fc85b3ed8efbea7a0eb55
noua026 <noua026@gmail.com>
2024-07-30 11:17:43 +0900

    cinema 작업

--- Ex06_MEGABOX/src/main/webapp/static/js/header.js
+++ Ex06_MEGABOX/src/main/webapp/static/js/header.js
@@ -1,6 +1,7 @@
+const data = document.querySelectorAll(".sup-content");
 const supListContents = document.querySelectorAll('.sup-list-content');
 const wrapper = document.querySelector(".header-wrapper");
-document.querySelectorAll(".sup-content").forEach((item,index)=>{
+data.forEach((item,index)=>{
     item.addEventListener('mouseover',function(e){
         supListContents.forEach(it=>it.style.display='none');
         // this.style.display='block';
@@ -17,9 +18,27 @@ document.querySelectorAll(".sup-content").forEach((item,index)=>{
           wrapper.style.display='none';
       })
 });
+// document.querySelector(".header-wrapper").addEventListener('mouseleave',function(){
+//   console.log(123456);
+//   supListContents.forEach(it=>it.style.display='none');
+//   this.style.display='none';
+// });
+const container = document.querySelector(".menu-img");
+container.addEventListener('click',function(){
+    const img = container.firstChild;
+    const menu = document.querySelector(".menu-container");
+    if(img.getAttribute('src').includes('menu')){
+        img.setAttribute('src','../static/images/cancel.svg');
+        menu.style.display='block';
+    }else{
+        img.setAttribute('src','../static/images/menu.svg');
+        menu.style.display='none';
+
+    }
+})
 
 
-// 검색 이미지
+// 검색 버튼
 const search = document.querySelector(".search-img");
 search.addEventListener('click',function(){
   if(search.children[0]['src'].includes('search')){
@@ -33,28 +52,11 @@ search.addEventListener('click',function(){
 document.querySelector("#closeLogin").addEventListener('click',function(){
   document.querySelector(".login-box").style.display='none';
   document.querySelector(".login-background").style.display='none';
-  document.querySelector(".main").style.overflow='auto';
 })
-
-// 로그인
+console.log(document.querySelector("#login"));
 document.querySelector("#login").addEventListener('click',function(){
   document.querySelector(".login-box").style.display='block';
   document.querySelector(".login-background").style.display='block';
   document.querySelector(".header-wrapper").style.display='none';
   supListContents.forEach(it=>it.style.display='none');
-  document.querySelector(".main").style.overflow='hidden';
-})
-// 메뉴 이미지
-const container = document.querySelector(".menu-img");
-container.addEventListener('click',function(){
-    const img = container.firstChild;
-    const menu = document.querySelector(".menu-container");
-    if(img.getAttribute('src').includes('menu')){
-        img.setAttribute('src','../static/images/cancel.svg');
-        menu.style.display='block';
-        menu.style.zIndex="2";
-    }else{
-        img.setAttribute('src','../static/images/menu.svg');
-        menu.style.display='none';	
-    }
 })
